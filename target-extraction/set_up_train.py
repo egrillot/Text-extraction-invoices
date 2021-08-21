0ae7c594edf8dec878c26274b30dd92150f008fc
egrillot <44230801+egrillot@users.noreply.github.com>
2021-08-21 10:46:09 +0100

    Add files via upload

---
+++ target-extraction/set_up_train.py
@@ -0,0 +1,85 @@
+from transformers.models.layoutlm import LayoutLMTokenizer
+from layoutlm.data.funsd import FunsdDataset, InputFeatures
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from torch.nn import CrossEntropyLoss
+from transformers import LayoutLMForTokenClassification
+import torch
+from tqdm import tqdm
+from transformers import AdamW
+
+dir_path='Desktop/VivaDrive/faktur_generator_v4/data/'
+
+def get_labels(path):
+    with open(path, "r") as f:
+        labels = f.read().splitlines()
+    if "O" not in labels:
+        labels = ["O"] + labels
+    return labels
+labels = get_labels(dir_path+'labels.txt')
+num_labels = len(labels)
+label_map = {i: label for i, label in enumerate(labels)}
+pad_token_label_id = CrossEntropyLoss().ignore_index
+
+args = {'local_rank': -1,
+        'overwrite_cache': True,
+        'data_dir': dir_path,
+        'model_name_or_path':'microsoft/layoutlm-base-uncased',
+        'max_seq_length': 512,
+        'model_type': 'layoutlmv2',}
+# class to turn the keys of a dict into attributes
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+args = AttrDict(args)
+tokenizer = LayoutLMTokenizer.from_pretrained("microsoft/layoutlm-base-uncased")
+# the LayoutLM authors already defined a specific FunsdDataset, so we are going to use this here
+train_dataset = FunsdDataset(args, tokenizer, labels, pad_token_label_id, mode="train")
+train_sampler = RandomSampler(train_dataset)
+train_dataloader = DataLoader(train_dataset,
+                              sampler=train_sampler,
+                              batch_size=2)
+eval_dataset = FunsdDataset(args, tokenizer, labels, pad_token_label_id, mode="test")
+eval_sampler = SequentialSampler(eval_dataset)
+eval_dataloader = DataLoader(eval_dataset,
+                             sampler=eval_sampler,
+                            batch_size=2)
+batch = next(iter(train_dataloader))
+input_ids = batch[0][0]
+tokenizer.decode(input_ids)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = LayoutLMForTokenClassification.from_pretrained("microsoft/layoutlm-base-uncased", num_labels=num_labels)
+model.to(device)
+
+optimizer = AdamW(model.parameters(), lr=5e-5)
+global_step = 0
+num_train_epochs = 1
+t_total = len(train_dataloader) * num_train_epochs # total number of training steps
+#put the model in training mode
+model.train()
+for epoch in range(num_train_epochs):
+  for batch in tqdm(train_dataloader, desc="Training"):
+      input_ids = batch[0].to(device)
+      bbox = batch[4].to(device)
+      attention_mask = batch[1].to(device)
+      token_type_ids = batch[2].to(device)
+      labels = batch[3].to(device)
+# forward pass
+      outputs = model(input_ids=input_ids, bbox=bbox, attention_mask=attention_mask, token_type_ids=token_type_ids,
+                      labels=labels)
+      loss = outputs.loss
+# print loss every 100 steps
+      if global_step % 100 == 0:
+        print(f"Loss after {global_step} steps: {loss.item()}")
+# backward pass to get the gradients 
+      loss.backward()
+#print("Gradients on classification head:")
+      #print(model.classifier.weight.grad[6,:].sum())
+# update
+      optimizer.step()
+      optimizer.zero_grad()
+      global_step += 1
+
+PATH='Desktop/VivaDrive/models/trained_layoutlm.pt'
+torch.save(model.state_dict(), PATH)
