19f7870930a4431f34e68bd8e4805e08470be7ed
egrillot <44230801+egrillot@users.noreply.github.com>
2021-08-22 20:14:50 +0100

    Update target_text_extraction.py

--- target-extraction/target_text_extraction.py
+++ target-extraction/target_text_extraction.py
@@ -20,10 +20,7 @@ class Tools:
 
     def label_map(labels):
         return {i: label for i, label in enumerate(labels)}
-
-    eng_month={'January':'01','February':'02','March':'03','April':'04','May':'05','June':'06','July':'07','August':'08','September':'09','October':'10','November':'11','December':'12'}
-    pol_month={'Styczeń':'01','Luty':'02','Marzec':'03','Kwiecień':'04','Maj':'05','Czerwiec':'06','Lipiec':'07','Sierpień':'08','Wrzesień':'09','Październik':'10','Listopad':'11','Grudzień':'12'}
-
+    
     def write_all_json(d,path):
         jfile=json.dumps(d)
         if len(path)!=0:
